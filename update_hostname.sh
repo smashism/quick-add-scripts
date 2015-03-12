9ea57a22352037161819c129c95338b442b9a69a
smashism <emily@modtitan.com>
2015-03-12 16:10:59 -0500

    Create update_hostname.sh

---
+++ update_hostname.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#
+# prompts IT member to enter computer name
+# updates hostname before binding to AD
+# adapted by github.com/shashism
+# last updated 2015-03-11
+#
+
+# Set CocoaDialog Location
+CD="/Library/Application Support/JAMF/bin/cocoaDialog.app/Contents/MacOS/cocoaDialog"
+
+# Dialog to enter the computer name and the create $ASSETTAG variable
+rv=($("$CD" standard-inputbox --title "Computer Name" --no-newline --informative-text "Enter the Computer Name (Based on Asset Tag)" --value-required))
+COMPNAME=${rv[1]}
+
+
+# Set Hostname using variable created above
+sudo scutil --set ComputerName $COMPNAME
+sudo scutil --set LocalHostName $COMPNAME
+sudo scutil --set HostName $COMPNAME
+
+
+# Dialog to confirm that the hostname was changed and what it was changed to.
+tb=`"$CD" ok-msgbox --text "Computer name updated!" \
+--informative-text "The computer's hostname has been added to $COMPNAME." \
+--no-newline --float`
+if [ "$tb" == "1" ]; then
+echo "User said OK"
+elif [ "$tb" == "2" ]; then
+echo "Canceling"
+exit
+fi
